39fc44fc5b8be583773246a1c536d60b46601b88
Skylar Onstot <skylar@themousepotato.net>
2019-05-30 15:35:04 -0700

    More constants

--- model.py
+++ model.py
@@ -4,7 +4,7 @@
 Each cell uses the following parameters:
 * Momentum tuple (U, V), defined by Navier-Stokes
 * Pressure value (P), defined by Navier-Stokes
-* Other water-related constants or behaviours found in research
+* Fractional Density value (D), defined by SOLA-VOF
 
 These values change as mass is pushed through the pipe.
 '''
@@ -16,6 +16,7 @@ N = 40
 grid = np.zeros((M,N,3))
 
 # Fluid constants
+flowRate = 1.0
 dx = 
 dy = 
 dt = 
@@ -23,19 +24,21 @@ T = 10
 d = 
 v = viscosity(T)
 
+# Boundary conditions (for walls, masses. etc.)
+
 
 # Finds value of Poisson's equation. If x == 0, this becomes Laplace's equation.
 def poisson(x):
 	return (((p[i+1,j]+p[i-1,j])*dy**2) + ((p[i,j+1]+p[i,j-1])*dx**2) - (x*dx**2*dy**2))/(2*(dx**2 + dy**2))
 
 # Discretized Navier-Stokes momentum/pressure equations. 'F' is a constant horizontal flow variable.
-def navierStokes(F,i,j):
+def navierStokes(i,j):
 	grid[i,j,0] = grid[i,j,0] - (grid[i,j,0] * dt / dx) * (grid[i,j,0] - grid[i-1,j,0])) -
 				    (grid[i,j,1] * (dt / dy) * (grid[i,j,0] - grid[i,j-1,0])) -
 				    ((dt / (2 * p * dx)) * (grid[i+1,j,2] - grid[i-1,j,2])) +
 				    (v * ((dt/dx**2) * (grid[i+1,j,0] - 2 * grid[i,j,0] + grid[i-1,j,0])) +
 					 ((dt/dy**2) * (grid[i,j+1,0] - 2 * grid[i,j,0] + grid[i,j-1,0])))+
-				    dt * F
+				    dt * flowRate
 	grid[i,j,1] = grid[i,j,1] - (grid[i,j,0] * (dt / dx) * (grid[i,j,1] - grid[i-1,j,1])) -
 				    (grid[i,j,1] * (dt / dy) * (grid[i,j,1] - grid[i,j-1,1])) -
 				    ((dt / (2 * p * dy)) * (grid[i,j+1,2] - grid[i,j-1,2])) +
@@ -45,14 +48,18 @@ def navierStokes(F,i,j):
 				(2 * (dx**2 + dy**2))) - (((p * (dx**2) * (dy**2))/ (2 * (dx**2 + dy**2))) *
 				())
 
-# Returns the dynamic viscosity of water, given a temperature value (in Celsius)
-def viscosity(t):
-	return 2.414e-5 * 10**(247.8/(t + 133.15))
+# Returns the kinematic viscosity of water, given a temperature value (in Celsius) and a density value
+def viscosity(density, t):
+	return (2.414e-5 * 10**(247.8/(t + 133.15))) / density
 
 # Water density given a Celsius temperature (DIPPR105 equation)
 def density(t):
 	return 0.14395 / 0.0112 ** (1 + (1 - ((t + 273.15) / 649.727)) ** 0.05107)
 
-# Sets up graphics window
-def setupGraphics():
-	w = GraphWin(width=200, height=200)
+# Simulates fluid density using the SOLA-VOF algorithm
+# Each cell has a fractional (0-1.0) density and is considered empty if density == 0
+# Free surface cells have density > 0 and density < 1
+# Must run after Navier-Stokes velocity/pressure simulation
+def fracVOF():
+	# dF/dT + drFu/dX + dFv/dY = 0
+	# dF = min(flux(A,D) * abs(V) + max(0, (1 - flux(A,D)) * abs(V) - (1 - flux(D)) * dX), flux(D) * dX)
